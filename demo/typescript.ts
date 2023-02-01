c8003fa193bf7e1923459ab1239f9607acea430d
Adam H.M <97729105+adamhmid@users.noreply.github.com>
2023-02-01 18:38:49 +0700

    initial commit

---
+++ demo/typescript.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+
+const world = "🗺️";
+console.log(world);
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
